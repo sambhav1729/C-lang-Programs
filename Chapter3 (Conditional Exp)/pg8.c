1f821f7ebdcd0ba7481bcf0b5b150abf42cfacbc
sambhav1729 <arorasambhav92@gmail.com>
2023-11-13 08:29:32 +0530

    Eigth commit

---
+++ Chapter3 (Conditional Exp)/pg8.c
@@ -0,0 +1,227 @@
+# include <stdio.h>
+
+int main()
+{
+    char letter;     // initialising letter variable
+    printf("Enter a character: ");
+    scanf("%c", &letter);  // taking a character as input from user
+
+    printf("\n");
+
+    switch (letter){    // checking lowercase or uppercase using switchcase statement
+        case 'A':
+            printf("It's a uppercase letter\n");
+            break;
+
+        case 'a':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'B':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'b':
+            printf("It's a lowercase letter\n");
+            break;
+  
+         case 'C':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'c':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'D':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'd':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'E':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'e':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'F':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'f':
+            printf("It's a lowercase letter\n");
+            break;
+        
+         case 'G':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'g':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'H':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'h':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'I':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'i':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'J':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'j':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'K':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'k':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'L':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'l':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'M':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'm':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'N':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'n':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'O':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'o':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'P':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'p':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'Q':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'q':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'R':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'r':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'S':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 's':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'T':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 't':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'U':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'u':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'V':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'v':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'W':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'w':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'X':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'x':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'Y':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'y':
+            printf("It's a lowercase letter\n");
+            break;
+
+         case 'Z':
+            printf("It's a uppercase letter\n");
+            break;
+
+         case 'z':
+            printf("It's a lowercase letter\n");
+            break;
+
+         default:
+            printf("Invalid input\n");
+            break;
+    }
+
+    return 0;
+}
+
